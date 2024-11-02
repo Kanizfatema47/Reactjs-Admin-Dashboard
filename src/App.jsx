b99333aab88ff3bb7c8ef6d5ef028b92316011de
Kaniz Fatema <kanizfatema0184@gmail.com>
2024-11-02 12:30:41 +0600

    route element , sidebar navigation link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
  
 import './App.css'
+import {Route,Routes} from "react-router-dom"
+import OverviewPage from './pages/OverviewPage'
+import ProductsPage from './pages/ProductsPage'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello</h1>
+      <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+        {/* BG */}
+        <div className='fixed inset-0 z-0'>
+          <div className='absolute inset-0 bg-gradiant-to-br from-gray-900 via-gjray-800 opacity-80'/>
+          <div className='absolute inset-0 backdrop-blur-sm'/>
+
+    
+
+        </div>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<OverviewPage/>}/>
+          <Route path="/products" element={<ProductsPage/>}/>
+        </Routes>
        </div>
     </>
   )
---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,9 @@
+
+
+const OverviewPage = () => {
+  return (
+    <div className="p-10 m-10">OverviewPage</div>
+  )
+}
+
+export default OverviewPage
